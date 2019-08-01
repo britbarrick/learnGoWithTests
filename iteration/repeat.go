3f905e921fa15f14e6f564930d59f49f293b049c
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 14:23:53 -0500

    refactor and added length selection feature

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,10 +2,10 @@ package iteration
 
 // Repeater accepts character in form of string, repeats character
 // five times.
-func Repeater(char string) string {
+func Repeater(char string, num int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + char
+	for i := 0; i < num; i++ {
+		repeated += char
 	}
 	return repeated
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,10 +3,16 @@ package iteration
 import "testing"
 
 func TestRepeater(t *testing.T) {
-	actual := Repeater("e")
-	expected := "eeeee"
+	actual := Repeater("e", 3)
+	expected := "eee"
 
 	if actual != expected {
 		t.Errorf("Expected %q, instead got %q.", expected, actual)
+	}
+}
+
+func BenchmarkRepeater(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeater("e,", 5)
 	}
 }
