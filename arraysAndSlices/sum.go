0248145af77f16cd17b97a0da72a29671e21a98a
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 14:55:11 -0500

    refactored sum function

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -3,8 +3,8 @@ package arraysandslices
 // Sum takes an array of 5 integers and returns the sum
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, num := range numbers {
+		sum += num
 	}
 	return sum
 }
