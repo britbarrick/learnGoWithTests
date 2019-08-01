43c1dc653c465ac62bde36244aa0d65c87346187
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 14:24:27 -0500

    changed function declaration comment

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,7 +1,7 @@
 package iteration
 
-// Repeater accepts character in form of string, repeats character
-// five times.
+// Repeater accepts character in form of string and a number, repeats character
+// the number of times chosen.
 func Repeater(char string, num int) string {
 	var repeated string
 	for i := 0; i < num; i++ {
