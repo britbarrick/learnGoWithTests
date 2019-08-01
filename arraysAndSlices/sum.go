a360e0f0dd4008f1bacadf7360c1e153914c37b9
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 15:11:36 -0500

    added aggregation of sums testing and functionality

--- arraysAndSlices/sum.go
+++ arraysAndSlices/sum.go
@@ -1,10 +1,23 @@
 package arraysandslices
 
 // Sum takes an array of 5 integers and returns the sum
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, num := range numbers {
 		sum += num
 	}
 	return sum
+}
+
+// SumAll takes a varying number of slices and returns a new
+// slice containing the totals for each slice passed in.
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNums := len(numbersToSum)
+	sums := make([]int, lengthOfNums)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -1,14 +1,27 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
 	actual := Sum(numbers)
 	expected := 15
 
 	if actual != expected {
 		t.Errorf("Expected %d, but got %d, %v", expected, actual, numbers)
+	}
+
+}
+
+func TestSumAll(t *testing.T) {
+	actual := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v but got %v.", expected, actual)
 	}
 }
