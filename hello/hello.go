b8da36f44a37b4e7b4fd9e617a3c143c16841632
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 13:28:54 -0500

    spanish language functionality added

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 // Hello passes in welcome string.
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,14 +12,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		actual := Hello("Chris")
+		actual := Hello("Chris", "")
 		expected := "Hello, Chris"
 		assertCorrectMessage(t, actual, expected)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		actual := Hello("")
+		actual := Hello("", "")
 		expected := "Hello, World"
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		actual := Hello("Elodie", "Spanish")
+		expected := "Hola, Elodie"
 		assertCorrectMessage(t, actual, expected)
 	})
 }
