bd92afd898ceb3a2e5564f4d742e5561814f9b44
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 14:53:30 -0500

    red to green - passed first test

---
+++ arraysAndSlices/sum.go
@@ -0,0 +1,10 @@
+package arraysandslices
+
+// Sum takes an array of 5 integers and returns the sum
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
