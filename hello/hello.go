28a32624e74d5e1e26a581e1bfcebfae75692ea5
Brit Barrick <brittney.barrick@centene.com>
2019-08-01 13:11:46 -0500

    refactored hello and hello_test

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello passes in welcome string.
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	actual := Hello("Chris")
-	expected := "Hello, Chris"
 
-	if actual != expected {
-		t.Errorf("Got %q, but expected %q.", actual, expected)
+	assertCorrectMessage := func(t *testing.T, actual, expected string) {
+		t.Helper()
+		if actual != expected {
+			t.Errorf("Got %q, but expected %q.", actual, expected)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		actual := Hello("Chris")
+		expected := "Hello, Chris"
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, World"
+		assertCorrectMessage(t, actual, expected)
+	})
 }
