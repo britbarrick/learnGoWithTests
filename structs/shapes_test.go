cb9e2ed7c4bb6b5a71c69819d7e7a3694e61fcfe
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 10:42:23 -0500

    refactored to utilize table tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -14,22 +14,19 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, expected float64) {
-		t.Helper()
-		actual := shape.Area()
-		if actual != expected {
-			t.Errorf("Expected %.2f, but got %.2f.", expected, actual)
-		}
+	areaTests := []struct {
+		shape    Shape
+		expected float64
+	}{
+		{Rectangle{5.0, 10.0}, 50.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 10.0}
-		checkArea(t, rectangle, 50.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		actual := tt.shape.Area()
+		if actual != tt.expected {
+			t.Errorf("Expected %.2f, but got %.2f.", tt.expected, actual)
+		}
+	}
 
 }
