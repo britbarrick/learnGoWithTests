32dc43b8141858b73cebce48a21efa126f999e30
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 11:28:20 -0500

    refactor test code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,30 +6,25 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t *testing.T, wallet Wallet, expected Bitcoin) {
+		t.Helper()
 		actual := wallet.Balance()
-		expected := Bitcoin(10)
 
 		if actual != expected {
 			t.Errorf("Expected %s, but got %s.", expected, actual)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{10}
-
 		wallet.Withdraw(Bitcoin(5))
-
-		actual := wallet.Balance()
-		expected := Bitcoin(5)
-
-		if actual != expected {
-			t.Errorf("Expected %s, but got %s.", expected, actual)
-		}
+		assertBalance(t, wallet, Bitcoin(5))
 	})
 
 }
