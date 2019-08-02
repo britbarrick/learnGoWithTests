56a5a7bf46277e3bdfda42f1bdb66068d7f0e0ec
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 13:16:34 -0500

    refactor first test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -8,6 +8,12 @@ func TestSearch(t *testing.T) {
 	actual := Search(dictionary, "test")
 	expected := "this is just a test"
 
+	assertStrings(t, actual, expected)
+}
+
+func assertStrings(t *testing.T, actual, expected string) {
+	t.Helper()
+
 	if actual != expected {
 		t.Errorf("Given: %q, expected %q, got %q.", "test", expected, actual)
 	}
