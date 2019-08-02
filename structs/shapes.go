87316a4871256da2a2117a9e05f445ef3fc9f65f
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 10:00:57 -0500

    rectangle area test and function written, test passed

--- structs/shapes.go
+++ structs/shapes.go
@@ -5,3 +5,9 @@ package structs
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
+
+// Area takes the width and height of a rectangle as float64 and
+// returns the area.
+func Area(width, height float64) float64 {
+	return width * height
+}
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("Expected %.2f, but got %.2f.", expected, actual)
 	}
 }
+
+func TestArea(t *testing.T) {
+	actual := Area(5.0, 10.0)
+	expected := 50.0
+
+	if actual != expected {
+		t.Errorf("Expected %.2v, but got %.2v.", expected, actual)
+	}
+}
