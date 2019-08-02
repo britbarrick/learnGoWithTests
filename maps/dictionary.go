751a2de31680f72236c70ed607d49c6b6fb45595
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 13:42:37 -0500

    added/passed test and function to evaluate existance of word and definition in dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,10 +1,16 @@
 package maps
 
+import "errors"
+
 // Dictionary is custom type declaration
 type Dictionary map[string]string
 
 // Search will comb through a map called dictionary for a word and
 // return a definition.
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("cannot find the word you're looking for")
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,24 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	actual := dictionary.Search("test")
-	expected := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		actual, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	assertStrings(t, actual, expected)
+		assertStrings(t, actual, expected)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		expected := "cannot find the word you're looking for"
+
+		if err == nil {
+			t.Fatal("expected an error.")
+		}
+
+		assertStrings(t, err.Error(), expected)
+	})
+
 }
 
 func assertStrings(t *testing.T, actual, expected string) {
