38e5d7db9219fff0c2c06af4b591debef56e017f
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 15:32:18 -0500

    added functionality for handling attempted addition of existing word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,7 +6,10 @@ import "errors"
 type Dictionary map[string]string
 
 // ErrNotFound removes potential for clashing changes between code and tests
-var ErrNotFound = errors.New("cannot find the word you're looking for")
+var (
+	ErrNotFound   = errors.New("cannot find the word you're looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 // Search will comb through a map called dictionary for a word and
 // return a definition.
@@ -19,6 +22,17 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // Add will append the new word and definition to the dictionary
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
