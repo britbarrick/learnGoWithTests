e5d07d2502a95a23a8c61d21e351fc3241f67bce
Brit Barrick <brittney.barrick@centene.com>
2019-08-02 13:19:44 -0500

    created custom type Dictionary, refactored accordingly

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,10 @@
 package maps
 
+// Dictionary is custom type declaration
+type Dictionary map[string]string
+
 // Search will comb through a map called dictionary for a word and
 // return a definition.
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
