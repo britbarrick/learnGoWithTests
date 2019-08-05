2ac4a0ba18045c12baeac8d3172195f8aa262e7a
Brit Barrick <brittney.barrick@centene.com>
2019-08-05 09:46:03 -0500

    added 1 sec pause between countdown calls

--- mocking/main.go
+++ mocking/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const (
@@ -15,8 +16,10 @@ const (
 // decrement and exiting upon hitting 0 and printing 'GO!'
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintf(out, "%v\n", i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 
 }
