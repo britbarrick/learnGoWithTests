06937a2947c47dbc9c830bb747cb448b9ad53a31
Brit Barrick <brittney.barrick@centene.com>
2019-08-05 09:44:14 -0500

    refactor, pulled out some constant vars

--- mocking/main.go
+++ mocking/main.go
@@ -6,13 +6,18 @@ import (
 	"os"
 )
 
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
 // Countdown begins at 3, waits one second between printing next
 // decrement and exiting upon hitting 0 and printing 'GO!'
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintf(out, "%v\n", i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 
 }
 
