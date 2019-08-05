899ff3dab4d11236e23ad35ce7890c37725b23fe
Brit Barrick <brittney.barrick@centene.com>
2019-08-05 08:40:06 -0500

    refactor and pass

--- di/di.go
+++ di/di.go
@@ -1,11 +1,20 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 // Greet takes a name, string, and returns a greeting
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
