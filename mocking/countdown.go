fb78c643469676bf6ffa1f50d9198ad3c35d36c5
Brit Barrick <brittney.barrick@centene.com>
2019-08-05 08:53:44 -0500

    wrote and passed first test

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,1 +1,15 @@
-package mocking
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Countdown begins at 3, waits one second between printing next
+// decrement and exiting upon hitting 0 and printing 'GO!'
+func Countdown(writer *bytes.Buffer) {
+	fmt.Fprint(writer, "3")
+}
+
+func main() {
+}
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,1 +1,19 @@
-package mocking
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	actual := buffer.String()
+	expected := "3"
+
+	if actual != expected {
+		t.Errorf("Expected %q but got %q", expected, actual)
+	}
+}
