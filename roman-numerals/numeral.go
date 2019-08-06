0b5e2a9bc5c62a30d0eaefd58b6ccdaf1f14b980
Brit Barrick <brittney.barrick@centene.com>
2019-08-06 13:54:10 -0500

    refactor, add functionality for conversion of 2

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -1,5 +1,8 @@
 package numerals
 
 func ConvertToRoman(num int) string {
+	if num == 2 {
+		return "II"
+	}
 	return "I"
 }
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,10 +3,39 @@ package numerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	actual := ConvertToRoman(1)
-	expected := "I"
-
-	if actual != expected {
-		t.Errorf("Expected %q but got %q", expected, actual)
+	cases := []struct {
+		Name     string
+		Arabic   int
+		Expected string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
 	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			actual := ConvertToRoman(test.Arabic)
+			if actual != test.Expected {
+				t.Errorf("Expected %q but got %q", test.Expected, actual)
+			}
+		})
+	}
+
+	t.Run("1 gets converted to I", func(t *testing.T) {
+		actual := ConvertToRoman(1)
+		expected := "I"
+
+		if actual != expected {
+			t.Errorf("Expected %q but got %q", expected, actual)
+		}
+	})
+
+	t.Run("2 gets converted to II", func(t *testing.T) {
+		actual := ConvertToRoman(2)
+		expected := "II"
+
+		if actual != expected {
+			t.Errorf("Expected %q but got %q", expected, actual)
+		}
+	})
 }
